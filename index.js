356f520b5e3fb9f67039580fef307f6f6afdd5dc
Jack Lach <jack.lach4@gmail.com>
2023-02-01 20:52:10 -0500

    invoked reducer() and brought 'totalBatteries' into global scope

--- index.js
+++ index.js
@@ -2,63 +2,14 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
+let totalBatteries; 
+
 const reducer = () => {
 
     const batteries = (accumulator, element) => { 
-        let totalBatteries = accumulator + element;
-        return totalBatteries;
+        return accumulator + element;
     }
-
-    return batteryBatches.reduce(batteries, 0);
+    totalBatteries = batteryBatches.reduce(batteries, 0);
 }
 
-
-
-            // const reducer = () => {
-
-            //     const batteries = (totalBatteries, element) => { 
-            //         return totalBatteries += element;
-            //         // return totalBatteries;
-            //     }
-
-            //     totalBatteries = batteryBatches.reduce(batteries, 0);
-                
-            // }
-
-// const batteries = () => {
-
-//         let totalBatteries;
-
-
-
-
-//     totalBatteries = batteryBatches.reduce(batteries, 0);
-//     return totalBatteries            
-// }
-
-
-
-
-            // const reducer = () => {
-
-            //     const batteries = (totalBatteries, element) => { 
-            //         return totalBatteries += element;
-            //         // return totalBatteries;
-            //     }
-
-            //     totalBatteries = batteryBatches.reduce(batteries, 0);
-            //     return totalBatteries;
-            // }
-
-
-// const reducer = () => {
-
-//     let totalBatteries;
-
-//     const batteries = (accumulator, element) => { 
-//         return accumulator + element;
-//     }
-
-//     totalBatteries = batteryBatches.reduce(batteries, 0);
-//     return totalBatteries;
-// }
+reducer(); // must invoke the function in the program for the value of totalBatteries to change.  Unlike other labs, these tests do not invoke the function for you.  
